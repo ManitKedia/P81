ee820221a3aeb8cff079398982f674dc0a0aa8c8
Shankar Weldmesh Pvt. Ltd <77322849+ManitKedia@users.noreply.github.com>
2021-09-21 12:59:34 +0530

    Add files via upload

--- C81main.js
+++ C81main.js
@@ -1,12 +1,32 @@
-canvas = document.getElementById("mycanvas");
-ctx = canvas.getContext("2d");
-
-color = "aqua";
+canvas = document.getElementById("my Canvas");
+ctx= canvas.getContext("2d");
 
 ctx.beginPath();
-
-ctx.strokeStyle = color;
-ctx.lineWidth = 2;
-ctx.arc(400, 200, 40, 0, 2*Math.PI);
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 5;
+ctx.arc(250, 210, 40 ,0 , 2*Math.PI);
 ctx.stroke();
-canvas.addEventListner("mousedown", my_mousedown);
+
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.lineWidth = 5;
+ctx.arc(350, 210, 40 ,0 , 2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.lineWidth = 5;
+ctx.arc(450, 210, 40 ,0 , 2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "orange";
+ctx.lineWidth = 5;
+ctx.arc(300, 250, 40 ,0 , 2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth = 5;
+ctx.arc(400, 250, 40 ,0 , 2*Math.PI);
+ctx.stroke();
